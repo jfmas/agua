a40aad37e94e7bef74066a945c205897c77c7fb0
JF Mas <jfmas@ciga.unam.mx>
2024-09-22 13:59:31 -0600

    22/09/2024

--- baja_sentinel.R
+++ baja_sentinel.R
@@ -1,4 +1,5 @@
 ## Sentinel 2 Proyecto PAPIIT Agua
+## Versión inutil hasta que sen2r se actualice  de de alta
 
 #("sen2r", dependencies = TRUE)
 setwd("/home/jf/pCloudDrive/proyectos/PAPIIT2023/Cointzio")
